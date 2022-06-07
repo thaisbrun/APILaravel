db7b8379511eff93eb8334ab06aefd66bef2c520
trava <travailthais@outlook.fr>
2022-06-07 17:03:30 +0200

    API en laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,13 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Service2;
-use App\Models\Inscrit;
-use App\Models\Jardinage;
-use App\Models\Menage;
-use App\Models\Transport;
 use Illuminate\Database\Seeder;
-
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,10 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-      //Inscrit::factory()->count(183)->create();
-      //Service2::factory()->count(198)->create();
-     //Transport::factory()->count(115)->create();
-      //Jardinage::factory()->count(100)->create();
-      //Menage::factory()->count(45)->create();
+        // \App\Models\User::factory(10)->create();
     }
 }
